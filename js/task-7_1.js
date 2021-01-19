7f04a8e897bacaa9bc7623d18b81fe3d276ac7f9
Pavel Sakun <axelhomkin@gmail.com>
2021-01-19 19:42:09 +0200

    comments

--- js/task-7_1.js
+++ js/task-7_1.js
@@ -35,10 +35,27 @@ const categoriesInfoRef = amountCategoriesRef.forEach(item =>
 // );
 
 //-------------------------
-// //
-// const testRef = categories.querySelector('.item');
+//
+const testRef = categories.querySelector('.item').childNodes;
 // const tray = amountCategoriesRef.childrenNodes;
-// console.log(testRef);
+console.log(testRef);
+
+const main = document.querySelector('#categories');
+console.log('main', main.children.length);
+
+/*
+elem.parentNode - выберет родителя elem
+elem.childNodes - псевдо-массив хранит все дочерние элементы, включая текстовые.
+elem.children - псевдо-массив хранит только дочерние узлы-элементы, то есть соответствующие тегам.
+elem.firstChild - выберет первый дочерний элемент внутри elem, включая текстовые узлы.
+elem.firstElementChild - выберет первый дочерний узел-элемент внутри elem.
+elem.lastChild - выберет последний дочерний элемент внутри elem, включая текстовые узлы.
+elem.lastElementChild - выберет последний дочерний узел-элемент внутри elem.
+elem.previousSibling - выберет элемент "слева" от elem (его предыдущего соседа)
+elem.previousElementSibling - выберет узел-элемент "слева" от elem (его предыдущего соседа)
+elem.nextSibling - выберет элемент "справа" от elem (его следующего соседа)
+elem.nextElementSibling - выберет узел-элемент "справа" от elem (его предыдущего соседа)
+*/
 
 // const titleRef = [...amountCategoriesRef].map(item => item > 'LI');
 // console.log(titleRef);
